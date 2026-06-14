03c393ac32ab86c1d7c3d09f51ece4d75ce5fb92
Jakub Lasecki <jakublasecki321@gmail.com>
2026-06-14 18:18:15 +0200

    added calculations

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import math
+import numpy as np
 
-print("hello worlds!")
+L = 39e-9  #  Murata LQW18AS39NG00D
+C1 = 0.4e-12  # BF199 transistor internal capacitance
+C2 =  4e-12  # variable capacitance
+
+
+carrier_freq = 1 / (2 * math.pi * np.sqrt(L * ((C1*C2)/(C1 + C2))))
+
+print(f"Frequency: {carrier_freq / 1e6} MHz")
